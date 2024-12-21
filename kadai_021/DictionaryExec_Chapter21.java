a7625b3160e586bdaef5a2567f9bd21d55418f0e
sato-haru-335 <totetotenonone@yahoo.co.jp>
2024-12-21 15:25:38 +0900

    Add files via upload

--- kadai_021/DictionaryExec_Chapter21.java
+++ kadai_021/DictionaryExec_Chapter21.java
@@ -5,12 +5,9 @@ public class DictionaryExec_Chapter21 {
 	public static void main(String[] args) {
 		Dictionary_Chapter21 checkStr = new Dictionary_Chapter21();
 
-		String arr[] = {"apple","banana","grape","orange"};
+		String[] arr = {"apple","banana","grape","orange"};
 		
-		for (int i = 0; i < arr.length; i++) {
-			checkStr.dictionary(arr[i]);
-			
-		}
+		checkStr.dictionary(arr);
 		
 	}
 
--- kadai_021/Dictionary_Chapter21.java
+++ kadai_021/Dictionary_Chapter21.java
@@ -3,7 +3,7 @@ import java.util.HashMap;
 
 public class Dictionary_Chapter21 {
 	
-	public void dictionary(String checkStr) {
+	public void dictionary(String[] checkStr) {
 		
 		HashMap<String, String> moji = new HashMap<String, String>();
 		
@@ -11,19 +11,22 @@ public class Dictionary_Chapter21 {
 		moji.put("peach", "桃");
 		moji.put("banana", "バナナ");
 		moji.put("lemon", "レモン");
-		moji.put("pear", "ピーチ");
+		moji.put("pear", "梨");
 		moji.put("kiwi", "キウイ");
 		moji.put("strawberry", "いちご");
 		moji.put("grape", "ぶどう");
 		moji.put("muscat", "マスカット");
 		moji.put("cherry", "さくらんぼ");
 		
-		if(moji.get(checkStr) != null) {
+		for (String word : checkStr) {
 			
-			System.out.println(moji.get(checkStr));
-		}
-		else {
-			System.out.println("辞書に存在していません");
+			if(moji.get(word) != null) {
+				System.out.println(word + "の意味は" +  moji.get(word));
+			}
+			else {
+				System.out.println(word +  "は辞書に存在しません");
+			}
+			
 		}
 		
 	}
